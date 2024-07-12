aabcaa62738801a9c7ccf67133cef89394e7b98a
timotiusdwimalau <142599354+timotiu@users.noreply.github.com>
2024-07-12 22:29:55 +0700

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,7 +14,7 @@ export function Home(props) {
       document.body.style.cursor = "default";
     }, [2000]);
     setNomor(e.target.parentNode.nextElementSibling.value);
-    const res = await axios.post("http://localhost:3001", {
+    const res = await axios.post("https://backend-pembangkit-soal-otomatis.vercel.app/", {
       nomor: e.target.parentNode.nextElementSibling.value,
       inputSoal,
       tipeSoal,
